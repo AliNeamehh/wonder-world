a7d03d94ece4a26365479047df0e783bec8402d7
AliNeamehh <alineamehh@gmail.com>
2024-11-03 18:29:40 +0200

    adding the detail for each card

--- detail.js
+++ detail.js
@@ -6,10 +6,29 @@ fetch('https://www.world-wonders-api.org/v0/wonders')
  .then(data=>{
      console.log("API Data:", data);
      const filtered_wondered=data.filter(wonder => wonder.name.toLowerCase() === wonderName.toLowerCase())
-     console.log(filtered_wondered);
+    if(filtered_wondered){
+        displayDetail(filtered_wondered[0])
+    }
+    
      
  })
+ .catch(error=>console.log(error));
 
- function displayDetail(data){
-    
+ function displayDetail(wonder){
+    console.log(wonder);
+    const detail_container=document.getElementById('container-detail');
+    console.log("container" ,detail_container);
+    detail_container.innerHTML=` <h2 class="wonder-title">${wonder.name}</h2>
+    <p class="wonder-summary">${wonder.summary}</p>
+     <p><strong>Location:</strong> ${wonder.location}</p>
+    <p><strong>Build Year:</strong> ${wonder.build_year}</p>
+    <p><strong>Time Period:</strong> ${wonder.time_period}</p>
+    <div class="links">
+      <a href="${wonder.links.wiki}" target="_blank">Wikipedia</a>
+      <a href="${wonder.links.britannica}" target="_blank">Britannica</a>
+      <a href="${wonder.links.google_maps}" target="_blank">Google Maps</a>
+      <a href="${wonder.links.trip_advisor}" target="_blank">TripAdvisor</a>
+    </div>
+    `;
+
  }
