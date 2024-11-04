4b9d5c0a9c8d420db643521297b19f4d5cec27dc
AliNeamehh <alineamehh@gmail.com>
2024-11-04 09:11:40 +0200

    change the order of image

--- detail.js
+++ detail.js
@@ -28,19 +28,19 @@ function displayDetail(wonder) {
    
      <div class="mySlides fade">
        <div class="numbertext">1 / 3</div>
-    <img src="${wonder.links.images[1]}" style="width:100%">
+    <img src="${wonder.links.images[0]}" style="width:100%">
       <div class="text">Caption Text</div>
      </div>
 
   <div class="mySlides fade">
     <div class="numbertext">2 / 3</div>
-    <img src="${wonder.links.images[2]}" style="width:100%">
+    <img src="${wonder.links.images[1]}" style="width:100%">
     <div class="text">Caption Two</div>
   </div>
 
   <div class="mySlides fade">
     <div class="numbertext">3 / 3</div>
-    <img src="${wonder.links.images[3]}" style="width:100%">
+    <img src="${wonder.links.images[2]}" style="width:100%">
     <div class="text">Caption Three</div>
   </div>
 
